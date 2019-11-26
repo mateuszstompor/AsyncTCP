87a69c4f2bcd343fba7c6d3ea0b139c1b19689d7
Mateusz Stompór <mateusz.stompor@gmail.com>
2019-11-26 13:13:22 +0100

    Add tests for partial send case

--- AsyncTCPTests/Common/Sanity/Server/ReceivingConnectionEventsTests.m
+++ AsyncTCPTests/Common/Sanity/Server/ReceivingConnectionEventsTests.m
@@ -38,6 +38,7 @@
     XCTestExpectation * clientDisconnectedExpectation;
     NSObject<ConnectionDelegate>* connectionHandler;
 }
+@property (atomic) Connection * connection;
 -(instancetype)initWithConnectionHandler: (NSObject<ConnectionDelegate>*) connectionHandler
               clientConnectedExpectation: (XCTestExpectation *) clientConnectedExpectation
            clientDisconnectedExpectation: (XCTestExpectation *) clientDisconnectedExpectation;
@@ -57,7 +58,8 @@
 }
 -(void)newClientHasConnected: (Connection *)connection {
     [clientConnectedExpectation fulfill];
-    connection.delegate = connectionHandler;
+    self.connection = connection;
+    self.connection.delegate = connectionHandler;
 }
 -(void)clientHasDisconnected:(Connection *)connection {
     [clientDisconnectedExpectation fulfill];
@@ -132,11 +134,57 @@
                                        clientDisconnectedExpectation:nil];
     server.delegate = serverHandler;
     [server boot];
-    XCTestExpectation * clientHasConnected = [self expectationForPredicate:[NSPredicate predicateWithFormat:@"connect >= 0"]
+    NSPredicate * clientHasConnectedPredicate = [NSPredicate predicateWithFormat:@"connect >= 0"];
+    XCTestExpectation * clientHasConnected = [self expectationForPredicate:clientHasConnectedPredicate
                                                        evaluatedWithObject:client
                                                                    handler:nil];
     [self waitForExpectations:@[clientHasConnected] timeout:3];
-    [client send:"hello" length:10];
+    [client send:"hellohello" length:10];
+    [self waitForExpectations:@[dataHasBeenReceived] timeout:6];
+    XCTAssertEqual([dataHasBeenReceived expectedFulfillmentCount], 1);
+    [client close];
+    [server shutDown:YES];
+}
+-(void)testCallbackIsNotReceivedPartialDataIsSent {
+    TCPTestsClient * client = [[TCPTestsClient alloc] initWithHost:"localhost" port:8091];
+    XCTestExpectation * dataHasBeenReceived = [[XCTestExpectation alloc]
+                                               initWithDescription:@"Callback informing about incoming data"];
+    connectionEventsHandler = [[ConnectionEventsHandler alloc]
+                               initWithDataReceivedExpectation:dataHasBeenReceived];
+    serverHandler = [[ServerHandler alloc] initWithConnectionHandler:connectionEventsHandler
+                                          clientConnectedExpectation:nil
+                                       clientDisconnectedExpectation:nil];
+    server.delegate = serverHandler;
+    [server boot];
+    NSPredicate * clientHasConnectedPredicate = [NSPredicate predicateWithFormat:@"connect >= 0"];
+    XCTestExpectation * clientHasConnected = [self expectationForPredicate:clientHasConnectedPredicate
+                                                       evaluatedWithObject:client
+                                                                   handler:nil];
+    [dataHasBeenReceived setInverted:YES];
+    [self waitForExpectations:@[clientHasConnected] timeout:3];
+    [client send:"hellohell" length:9];
+    [self waitForExpectations:@[dataHasBeenReceived] timeout:6];
+    [client send:"o" length:1];
+    [client close];
+    [server shutDown:YES];
+}
+-(void)testCallbackIsReceivedWhenFullDataIsSentPartially {
+    TCPTestsClient * client = [[TCPTestsClient alloc] initWithHost:"localhost" port:8091];
+    XCTestExpectation * dataHasBeenReceived = [[XCTestExpectation alloc]
+                                               initWithDescription:@"Callback informing about incoming data"];
+    connectionEventsHandler = [[ConnectionEventsHandler alloc] initWithDataReceivedExpectation:dataHasBeenReceived];
+    serverHandler = [[ServerHandler alloc] initWithConnectionHandler:connectionEventsHandler
+                                          clientConnectedExpectation:nil
+                                       clientDisconnectedExpectation:nil];
+    server.delegate = serverHandler;
+    [server boot];
+    NSPredicate * clientHasConnectedPredicate = [NSPredicate predicateWithFormat:@"connect >= 0"];
+    XCTestExpectation * clientHasConnected = [self expectationForPredicate:clientHasConnectedPredicate
+                                                       evaluatedWithObject:client
+                                                                   handler:nil];
+    [self waitForExpectations:@[clientHasConnected] timeout:3];
+    [client send:"hellohell" length:9];
+    [client send:"hellohell" length:9];
     [self waitForExpectations:@[dataHasBeenReceived] timeout:6];
     XCTAssertEqual([dataHasBeenReceived expectedFulfillmentCount], 1);
     [client close];
