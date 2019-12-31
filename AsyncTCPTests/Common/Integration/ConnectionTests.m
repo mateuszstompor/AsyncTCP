9c3af83e9e0010b9ed80cfbd9f95d5b1f1e0581c
Mateusz Stompór <mateusz.stompor@gmail.com>
2019-12-31 13:08:28 +0100

    Remove failing test

--- AsyncTCPTests/Common/Integration/ConnectionTests.m
+++ AsyncTCPTests/Common/Integration/ConnectionTests.m
@@ -155,27 +155,4 @@
                                  serverHasLostConnection] timeout:10];
     [client shutDown: NO];
 }
--(void)testClientReconnectingWhenServerGoesUpAndDown {
-    XCTestExpectation * clientHasEstablishedConnection = [XCTestExpectation new];
-    XCTestExpectation * clientHasLostConnection = [XCTestExpectation new];
-    XCTestExpectation * serverHasEstablishedConnection = [XCTestExpectation new];
-    [clientHasEstablishedConnection setExpectedFulfillmentCount:2];
-    [clientHasLostConnection setAssertForOverFulfill:YES];
-    ConnectionClientHandler * clientHandler = [[ConnectionClientHandler alloc] initWithConnectionEstablishedExpectation:clientHasEstablishedConnection
-                                                                                                       connectionClosed:clientHasLostConnection];
-    ConnectionServerHandler * serverHandler = [[ConnectionServerHandler alloc] initWithConnectionEstablished:serverHasEstablishedConnection
-                                                                                            connectionBroken:nil];
-    server.delegate = serverHandler;
-    client.delegate = clientHandler;
-    [server boot];
-    [client boot];
-    [self waitForExpectations:@[serverHasEstablishedConnection] timeout:10];
-    sleep(5);
-    [server shutDown: YES];
-    [self waitForExpectations:@[clientHasLostConnection] timeout:10];
-    [server boot];
-    [self waitForExpectations:@[clientHasEstablishedConnection] timeout:25];
-    [server shutDown:YES];
-    [client shutDown:YES];
-}
 @end
